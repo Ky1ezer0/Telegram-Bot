7c3c9171874db36181a3e6413285941f3567f573
Kylez <kylezli@protonmail.com>
2021-08-18 17:18:31 +0800

    Added test cases

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("1940148984:AAELX6QJlYiSplwpmb4_dzsvGz04ryZUjDA")
 
 @bot.message_handler(commands=['start','help'])
 def weather(message):
-    text = "Hello! This Bot is still in Beta! Current available commands are: /weather, /car"
+    text = "Hello! This Bot is still in Beta! Current available commands are: /weather, /car. There has a hidden function to my gf to calulate the time."
     bot.reply_to(message, text)
 
 @bot.message_handler(commands=['weather'])
@@ -35,5 +35,19 @@ def weather(message):
         for info in article_rev[-10:]:
             carInfo += info.text+"\n\n"
     bot.reply_to(message, carInfo)
+
+@bot.message_handler(func=lambda message: message.text is not None and len(message.text) == 11 and ':' in message.text and '-' in message.text)
+def cal_time(message):
+    try:
+        start_time, end_time = message.text.split('-')
+        s_h, s_m= start_time.split(':')
+        e_h, e_m= end_time.split(':')
+        start_time = dt.datetime.strptime(start_time,'%H:%M')
+        end_time = dt.datetime.strptime(end_time,'%H:%M')
+        result = end_time - start_time
+        bot.reply_to(message, result)
+    except ValueError:
+        result = "It seems not a correct format."
+        bot.reply_to(message, result)
 
 bot.polling()
